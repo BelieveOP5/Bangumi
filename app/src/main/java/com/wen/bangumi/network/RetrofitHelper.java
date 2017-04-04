f0f3cba215c9f9c3a15ff42786d8ac625b38660c
BelieveOP5 <zhenbowen888@outlook.com>
2017-04-04 19:30:25 +0800

    Change Something.

--- app/src/main/java/com/wen/bangumi/network/RetrofitHelper.java
+++ app/src/main/java/com/wen/bangumi/network/RetrofitHelper.java
@@ -1,15 +1,12 @@
 package com.wen.bangumi.network;
 
 import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
-import com.wen.bangumi.network.api.bilibili.BiliBiliApi;
 import com.wen.bangumi.network.api.bangumi.BangumiApi;
+import com.wen.bangumi.network.api.bilibili.BiliBiliApi;
 
-import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
-import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
-import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
